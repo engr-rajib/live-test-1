baf9b7af7a12e050f60fa833e81c0b4545e164e3
engr-rajib <engr.rajibb@gmail.com>
2025-01-01 21:24:22 +0600

    Updated

--- bin/live_test_1.dart
+++ bin/live_test_1.dart
@@ -1,5 +1,3 @@
 void main() {
- for (int i = 0; i <= 4; i++) {
-   print('Hello world!');
- }
+
 }
--- lib/live_test_1.dart
+++ lib/live_test_1.dart
@@ -1,7 +1,3 @@
 void main () {
-  List<String> name = ["Rajib", "Shuva", "Soumya", "Shourjo"];
 
-  for (int i = 0; i < name.length; i++) {
-    print("Person-$i name is ${name[i]}.");
-  }
 }
