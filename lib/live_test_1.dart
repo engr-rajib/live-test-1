ad18a6ba26b391169b6bef63b2a60aab7c1da0da
engr-rajib <engr.rajibb@gmail.com>
2025-01-01 21:20:05 +0600

    Updated

--- lib/live_test_1.dart
+++ lib/live_test_1.dart
@@ -1,3 +1,7 @@
-int calculate() {
-  return 6 * 7;
+void main () {
+  List<String> name = ["Rajib", "Shuva", "Soumya", "Shourjo"];
+
+  for (int i = 0; i < name.length; i++) {
+    print("Person-$i name is ${name[i]}.");
+  }
 }
