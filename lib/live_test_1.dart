c401b84d18763247b57fe170e2260058490c4d87
engr-rajib <engr.rajibb@gmail.com>
2025-01-01 23:08:15 +0600

    Updated

--- lib/live_test_1.dart
+++ lib/live_test_1.dart
@@ -1,3 +1,11 @@
 void main () {
+double triangleBase = 12.0;
+double triangleHeight = 5.5;
+double triangleArea = triangle(triangleBase, triangleHeight);
+print("The area of the triangle is $triangleArea");
+}
 
+double triangle (double base, double height) {
+  double area = 0.5 * base * height;
+  return area;
 }
--- test/live_test_1_test.dart
+++ test/live_test_1_test.dart
@@ -2,7 +2,5 @@ import 'package:live_test_1/live_test_1.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+
 }
